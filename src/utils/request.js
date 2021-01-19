c496eb4297a8f6c85d2860e576c1f4ca712f6504
lxy93 <1406062098@qq.com>
2021-01-19 18:02:49 +0800

    登录账号的修改

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const service = axios.create();创建axios实例
+const service = axios.create();//创建axios实例
 
 service.interceptors.request.use(); //请求拦截器
 
--- src/utils/validate.js
+++ src/utils/validate.js
@@ -18,3 +18,8 @@ export function validUsername(str) {
   const valid_map = ['admin', 'editor']
   return valid_map.indexOf(str.trim()) >= 0
 }
+// 验证手机号码
+export function validMobile(str) {
+  const reg = /^1[3-9]\d{9}$/;
+  return reg.test(str)
+}
