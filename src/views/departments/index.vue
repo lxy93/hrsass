3505a64427fc6cb3b2673a55dded82c283d38574
lxy93 <1406062098@qq.com>
2021-01-26 22:30:16 +0800

    组织架构头部的编写完成

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,9 +1,29 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        组织架构
-      </h2>
+      <el-card>
+        <el-row type="flex" justify="space-between" align="middle" style="height:40px;">
+          <el-col>
+            xxx科技有限公司
+          </el-col>
+          <el-col :span="4">
+            <el-row type="flex">
+              <el-col>负责人</el-col>
+              <el-col>
+                <el-dropdown>
+                  <span>
+                    操作
+                    <i class="el-icon-arrow-down"></i>
+                  </span>
+                  <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item>添加</el-dropdown-item>
+                  </el-dropdown-menu>
+                </el-dropdown>
+              </el-col>
+            </el-row>
+          </el-col>
+        </el-row>
+      </el-card>
     </div>
   </div>
 </template>
