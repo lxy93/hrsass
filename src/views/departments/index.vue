ccd716963fb93da1d3cc05ee21c6fb7b2e0e1d30
lxy93 <1406062098@qq.com>
2021-01-26 23:09:39 +0800

    静态自定义tree 结构完成

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -2,7 +2,7 @@
   <div class="dashboard-container">
     <div class="app-container">
       <el-card>
-        <el-row type="flex" justify="space-between" align="middle" style="height:40px;">
+        <el-row type="flex" justify="space-between" align="middle" style="height:40px;margin-bottom:30px;">
           <el-col>
             xxx科技有限公司
           </el-col>
@@ -23,6 +23,33 @@
             </el-row>
           </el-col>
         </el-row>
+
+        <!-- 树形结构开始 -->
+        <el-tree :data="departs" :props="defaultProps" :default-expand-all="true">
+          <el-row slot-scope="{ data }" type="flex" justify="space-between" align="middle" style="height:40px;width:100%;">
+            <el-col>
+              {{data.name}}
+            </el-col>
+            <el-col :span="4">
+              <el-row type="flex">
+                <el-col>{{data.manager}}</el-col>
+                <el-col>
+                  <el-dropdown>
+                    <span>
+                      操作
+                      <i class="el-icon-arrow-down"></i>
+                    </span>
+                    <el-dropdown-menu slot="dropdown">
+                      <el-dropdown-item>添加1</el-dropdown-item>
+                      <el-dropdown-item>添加2</el-dropdown-item>
+                      <el-dropdown-item>添加3</el-dropdown-item>
+                    </el-dropdown-menu>
+                  </el-dropdown>
+                </el-col>
+              </el-row>
+            </el-col>
+          </el-row>
+        </el-tree>
       </el-card>
     </div>
   </div>
@@ -30,7 +57,34 @@
 
 <script>
 export default {
-
+  data(){
+    return {
+      departs: [
+        {
+          name: '总裁办',
+          manager:'曹操',
+          children: [
+            { 
+              name: '董事会',
+              manager:'曹丕'
+            }
+          ]
+        },
+        {
+          name: '行政部',
+          manager:'刘备'
+        },
+        { 
+          name: '人事部',
+          manager:'孙权'
+        }
+      ],
+      defaultProps:{
+        label:'name',
+        children:'children'
+      }
+    }
+  }
 }
 </script>
 
